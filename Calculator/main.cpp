1737510706c273896e35074d7c1cb4255d88f7ef
Alexis Fernando Rodriguez <alexisfernando@Alexiss-MacBook-Pro.local>
2017-11-02 01:26:36 +0000

    first commit

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -7,9 +7,38 @@
 //
 
 #include <iostream>
+#include "stdio.h"
+int sumar(int numenoUno, int numeroDos);
+int restar(int numeroUno, int numeroDos);
+int multiplicar(int numeroUno, int numeroDos);
+int dividir(int numeroUno, int numeroDos);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int primerNumero = 5;
+    int segundoNumero = 0;
+    printf("La suma de %d y %d es: %d \n",primerNumero, segundoNumero, sumar(primerNumero, segundoNumero));
+    printf("La resta de %d y %d es: %d \n",primerNumero, segundoNumero, restar(primerNumero, segundoNumero));
+    printf("La multiplicacion de %d y %d es: %d \n",primerNumero, segundoNumero, multiplicar(primerNumero, segundoNumero));
+    printf("La division de %d y %d es: %d \n",primerNumero, segundoNumero, dividir(primerNumero, segundoNumero));
+    
     return 0;
+}
+
+int sumar(int numeroUno, int numeroDos){
+    return numeroUno + numeroDos;
+}
+
+int restar(int numeroUno, int numeroDos){
+    return numeroUno - numeroDos;
+}
+
+int multiplicar(int numeroUno, int numeroDos){
+    return numeroUno * numeroDos;
+}
+
+int dividir(int numeroUno, int numeroDos){
+    if(numeroDos == 0){
+        return -1;
+    }
+    return numeroUno / numeroUno;
 }
